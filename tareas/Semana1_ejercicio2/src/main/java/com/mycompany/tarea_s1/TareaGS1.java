f36e0a689931c92e1a14965d5f455921cef415f4
GuadalupeEmanuel <r01068d@upla.edu.pe>
2024-11-01 11:14:07 -0500

    Add files via upload

---
+++ tareas/Semana1_ejercicio2/src/main/java/com/mycompany/tarea_s1/TareaGS1.java
@@ -0,0 +1,12 @@
+package com.mycompany.tarea_s1;
+
+import Vista.Grafico;
+
+public class TareaGS1 {
+
+    public static void main(String[] args) {
+        Grafico tarea = new Grafico();
+        tarea.show();
+
+    }
+}
---
+++ tareas/semana1_ejercicio1/src/main/java/com/mycompany/tarea1/TAREA1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea1;
+
+import VISTA.TAREA2;
+
+/**
+ *
+ * @author Sergio
+ */
+public class TAREA1 {
+
+    public static void main(String[] args) {
+     TAREA2 tarea = new TAREA2();
+   tarea.show();
+    }
+}
