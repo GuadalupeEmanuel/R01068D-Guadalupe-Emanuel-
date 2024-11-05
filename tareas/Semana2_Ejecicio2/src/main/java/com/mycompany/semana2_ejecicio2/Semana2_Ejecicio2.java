a864df8918d28bdc7d0f021c3f1d5541d7e53a75
GuadalupeEmanuel <r01068d@upla.edu.pe>
2024-11-04 20:16:25 -0500

    Add files via upload

---
+++ tareas/Semana2_Ejecicio2/src/main/java/com/mycompany/semana2_ejecicio2/Semana2_Ejecicio2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana2_ejecicio2;
+
+import Vista.Grafico;
+
+/**
+ *
+ * @author Elvis
+ */
+public class Semana2_Ejecicio2 {
+
+    public static void main(String[] args) {
+         Grafico rubro=new Grafico();
+        rubro.setVisible(true);
+    }
+}
---
+++ tareas/Semana2_ejercicio1/src/main/java/com/mycompany/semana2_ejercicio1/Semana2_ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana2_ejercicio1;
+
+import Vista.Grafico;
+
+/**
+ *
+ * @author Elvis
+ */
+public class Semana2_ejercicio1 {
+
+    public static void main(String[] args) {
+        Grafico rubro=new Grafico();
+        rubro.setVisible(true);
+    }
+}
